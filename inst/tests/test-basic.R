3a724adf4522cd341679377322bbd50b8812de5d
Carlos Scheidegger <cscheid@research.att.com>
2013-02-08 17:00:12 -0500

    more tests

--- inst/tests/test-basic.R
+++ inst/tests/test-basic.R
@@ -4,8 +4,11 @@ context("basic sanity checks.")
 repo.path <- "/Users/cscheid/code/guitar/.git"
 
 test_that("repositories can be loaded", {
-  
   expect_that(new(guitar::Repository, repo.path), is_a("Rcpp_Repository"))
+})
+
+test_that("bad repositories throw", {
+  expect_that(new(guitar::Repository, "this should never work"), throws_error("Error : Repository not found"))
 })
 
 test_that("repositories have indices", {
@@ -17,4 +20,7 @@ test_that("references can be found by name", {
   repo <- new(guitar::Repository, repo.path)
   ref <- repo$reference_lookup("refs/heads/master")
   expect_that(ref, is_a("Rcpp_Reference"))
+  oid <- repo$name_to_id("refs/heads/master")
+  expect_that(oid, is_a("Rcpp_OID"))
+  expect_that(oid$fmt(), is_a("character"));
 })
