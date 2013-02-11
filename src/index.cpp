799d1a2e397b1725793542c03188326fb9a9ea27
Carlos Scheidegger <cscheid@research.att.com>
2013-02-10 22:50:14 -0500

    more methods on index

--- src/index.cpp
+++ src/index.cpp
@@ -1,5 +1,7 @@
 #include "index.h"
 #include "entry_points.h"
+#include "tree.h"
+#include "oid.h"
 
 using namespace Rcpp;
 
@@ -30,6 +32,23 @@ SEXP Index::write()
     END_RCPP
 }
 
+void Index::read_tree(SEXP _tree)
+{
+    const git_tree *tree = Tree::from_sexp(_tree);
+    int err = _git_index_read_tree(ix.get(), tree);
+    if (err)
+        throw Rcpp::exception("read_tree error");
+}
+
+Rcpp::Reference Index::write_tree()
+{
+    git_oid result;
+    int err = _git_index_write_tree(&result, ix.get());
+    if (err)
+        throw Rcpp::exception("write_tree error");
+    return OID::create(&result);
+}
+
 size_t Index::entrycount()
 {
     return _git_index_entrycount(ix.get());
@@ -40,11 +59,37 @@ void Index::clear()
     _git_index_clear(ix.get());
 }
 
+void Index::add_by_path(std::string path)
+{
+    int err = _git_index_add_bypath(ix.get(), path.c_str());
+    if (err)
+        throw Rcpp::exception("add_by_path failed");
+}
+
+void Index::remove_by_path(std::string path)
+{
+    int err = _git_index_remove_bypath(ix.get(), path.c_str());
+    if (err)
+        throw Rcpp::exception("remove_by_path failed");
+}
+
+void Index::remove_directory(std::string path, int stage)
+{
+    int err = _git_index_remove_directory(ix.get(), path.c_str(), stage);
+    if (err)
+        throw Rcpp::exception("remove_directory failed");
+}
+
 RCPP_MODULE(guitar_index) {
     class_<Index>("Index")
         .method("read", &Index::read)
         .method("write", &Index::write)
+        .method("write_tree", &Index::write_tree)
         .method("entrycount", &Index::entrycount)
         .method("clear", &Index::clear)
+        .method("add_by_path", &Index::add_by_path)
+        .method("remove_by_path", &Index::remove_by_path)
+        .method("remove_directory", &Index::remove_directory)
+        .method("read_tree", &Index::read_tree)
         ;
 }
--- src/index.h
+++ src/index.h
@@ -9,8 +9,6 @@ public:
     explicit Index(git_index *_ix); 
 
     // GIT_EXTERN(int) git_index_open(git_index **out, const char *index_path);
-    // GIT_EXTERN(int) git_index_new(git_index **out);
-    // GIT_EXTERN(void) git_index_free(git_index *index);
     // GIT_EXTERN(git_repository *) git_index_owner(const git_index *index);
     // GIT_EXTERN(unsigned int) git_index_caps(const git_index *index);
     // GIT_EXTERN(int) git_index_set_caps(git_index *index, unsigned int caps);
@@ -18,27 +16,26 @@ public:
     SEXP read(); // git_index_read
     SEXP write(); // git_index_write
 
-    // GIT_EXTERN(int) git_index_read_tree(git_index *index, const git_tree *tree);
-    // GIT_EXTERN(int) git_index_write_tree(git_oid *out, git_index *index);
+    void read_tree(SEXP tree);
+    Rcpp::Reference write_tree(); // returns OID of the tree, not tree object itself
+
     // GIT_EXTERN(int) git_index_write_tree_to(git_oid *out, git_index *index, git_repository *repo);
 
     size_t entrycount(); // git_index_entrycount
     void clear(); // git_index_clear;
 
-    // GIT_EXTERN(const git_index_entry *) git_index_get_byindex(
-    //     git_index *index, size_t n);
-    // GIT_EXTERN(const git_index_entry *) git_index_get_bypath(
-    //                                                          git_index *index, const char *path, int stage);
+    // GIT_EXTERN(const git_index_entry *) git_index_get_byindex(git_index *index, size_t n);
+    // GIT_EXTERN(const git_index_entry *) git_index_get_bypath(git_index *index, const char *path, int stage);
     // GIT_EXTERN(int) git_index_remove(git_index *index, const char *path, int stage);
-    // GIT_EXTERN(int) git_index_remove_directory(
-    //                                            git_index *index, const char *dir, int stage);
     // GIT_EXTERN(int) git_index_add(git_index *index, const git_index_entry *source_entry);
     // GIT_EXTERN(int) git_index_entry_stage(const git_index_entry *entry);
-    // GIT_EXTERN(int) git_index_add_bypath(git_index *index, const char *path);
-    // GIT_EXTERN(int) git_index_remove_bypath(git_index *index, const char *path);
+
+    void add_by_path(std::string path);
+    void remove_by_path(std::string path);
+    void remove_directory(std::string path, int stage);
+
     // GIT_EXTERN(int) git_index_find(size_t *at_pos, git_index *index, const char *path);
-    // GIT_EXTERN(int) git_index_conflict_add(
-    //                                        git_index *index,
+    // GIT_EXTERN(int) git_index_conflict_add(git_index *index,
     //                                        const git_index_entry *ancestor_entry,
     //                                        const git_index_entry *our_entry,
     //                                        const git_index_entry *their_entry);
